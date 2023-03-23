223a2b429b37f13be4ad167c22cb2e11609f78ed
Alasgar M <mahmudov.alasgar@gmail.com>
2023-03-24 03:14:38 +0400

    add swagger documentation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -36,8 +41,13 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-  res.status(200).send('jobs api');
+  res
+    .status(200)
+    .send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
